18d926f177a9f2566ef8723de73a1c92d4ca3985
grizzle <firbigi1993@gmail.com>
2019-09-27 23:18:37 +0900

    og tag

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,17 +1,16 @@
-console.log("blog js")
+
 
 let articles = {};
 
 _get = () => {
-    console.log("get");
     // 20개 까지 우선 출력
-    return fetch('https://api.brunch.co.kr/v2/article/@arosaegle')
-    .then(res => res.json())
-    .then(res => res.data.list)
-    .catch(err => console.log(err))
+    return fetch('https://api.brunch.co.kr/v2/article/@modernmother')
+        .then(res => res.json())
+        .then(res => res.data.list)
+        .catch(err => console.log(err))
 }
 
-_getArticle = async () =>{
+_getArticle = async () => {
     articles = await _get();
     drawPosts();
 }
@@ -22,23 +21,23 @@ drawPosts = () => {
     const posts = document.querySelector('.posts');
     const li_post = document.querySelector('.post');
     const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
+        "July", "August", "September", "October", "November", "December"
+    ];
 
 
-    articles.map(post =>{
+    articles.map(post => {
         // get data from url
         const thumb = post.articleImageForDiscover ? post.articleImageForDiscover : post.articleImageList[0].url;
         const title = post.title;
         const subtitle = post.subTitle;
         const date_temp = new Date(post.updateTime);
-        const date = monthNames[date_temp.getMonth()] + " " +date_temp.getDay().toString()+ "," +   date_temp.getFullYear().toString();
+        const date = monthNames[date_temp.getMonth()] + " " + date_temp.getDay().toString() + "," + date_temp.getFullYear().toString();
 
         const summary = post.contentSummary;
         const summary_arr = summary.split('.');
         const summary_first = summary_arr[0];
-        const summary_last = summary_arr[summary_arr.length -2];        
-        
+        const summary_last = summary_arr[summary_arr.length - 2];
+
         const url = "https://brunch.co.kr/@arosaegle/" + post.no;
 
         // add child
@@ -55,7 +54,7 @@ drawPosts = () => {
         const post_data = p.querySelector('.post_data');
         const post_summary_first = p.querySelector('.post_summary_first');
         const post_summary_last = p.querySelector('.post_summary_last');
-        
+
 
         post_url.href = url;
         post_img.style.backgroundImage = "url(" + thumb + ")";
@@ -64,7 +63,7 @@ drawPosts = () => {
         post_data.innerHTML = date;
         post_summary_first.innerHTML = "<span>[0] </span>" + summary_first;
         post_summary_last.innerHTML = "<span>[-1] </span>" + summary_last;
-        
+
         posts.appendChild(p);
     })
 
